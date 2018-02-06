c8459ac45280b01fee72a68b26d225a4ea2c2cf4
Juan Fumero <juan.fumero@ed.ac.uk>
2018-02-06 09:53:20 +0100

    Default paths fot CUDA

---
+++ bash/cudaPaths.sh
@@ -0,0 +1,5 @@
+export CPLUS_INCLUDE_PATH=/usr/local/cuda/include
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64
+#export LD_LIBRARY_PATH=/usr/lib64/nvidia/:$LD_LIBRARY_PATH
+export PATH=/usr/local/cuda/bin/:$PATH
+

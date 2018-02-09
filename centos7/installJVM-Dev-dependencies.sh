23ad55ecba21f0b098b4406eac9b4f08709e353b
Juan Fumero <juan.fumero@ed.ac.uk>
2018-02-09 18:07:58 +0100

    Packages for compiling JVM and Graal on CentOS

---
+++ centos7/installJVM-Dev-dependencies.sh
@@ -0,0 +1,4 @@
+
+sudo yum install libXtst-devel libXt-devel libXrender-devel cups-devel alsa-lib-devel ccache 
+sudo yum install glibc-devel libstdc++-devel  ## x64 version
+sudo yum install glibc-devel.i686 libstdc++-devel.i686

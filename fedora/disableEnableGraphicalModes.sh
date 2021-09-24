255d1e9afbb4fc1c79db66e08b54f1a887f5c55d
Juan Fumero <jjfumero@gmail.com>
2021-09-24 10:53:28 +0200

    management fedora scripts

---
+++ fedora/disableEnableGraphicalModes.sh
@@ -0,0 +1,10 @@
+
+## Disable graphical mode
+
+sudo systemctl set-default multi-user.target
+
+
+## Enable graphical interface
+sudo systemctl set-default graphical.target
+
+
---
+++ fedora/loadWifiModule-XPS17.sh
@@ -0,0 +1,1 @@
+sudo modprobe ath11k_pci
